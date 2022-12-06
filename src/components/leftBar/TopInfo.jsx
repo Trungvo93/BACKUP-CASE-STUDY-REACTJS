9d9e62ae95a9cd8873b3b1df59818495b3c1d2ca
Trungvo93 <100949580+Trungvo93@users.noreply.github.com>
2022-12-06 23:01:25 +0700

    .

---
+++ src/components/leftBar/TopInfo.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import { useCookies } from "react-cookie";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import "./TopInfo.css";
+const TopInfo = () => {
+  const [cookies] = useCookies();
+  const users = useSelector((state) => state.users);
+  const [loginedUser, setLoginedUser] = useState({});
+  useEffect(() => {
+    const infoLoginUser = users.find(
+      (item) => item.username === cookies.username
+    );
+    // console.log(loginedUser.hasOwnProperty("username"));
+    setLoginedUser({ ...infoLoginUser });
+  }, [users]);
+  const navigate = useNavigate();
+  const gotoDashboard = () => {
+    navigate("/home/dashboard");
+  };
+  return (
+    <>
+      <div className="d-none d-xl-block">
+        <img
+          src={`${process.env.PUBLIC_URL}/images/logo_library.png`}
+          alt=""
+          className="w-100 logo_library"
+          onClick={gotoDashboard}
+        />
+      </div>
+      <div className="bg-secondary avatarLogin p-3 mx-3  rounded-3  logo   d-none d-xl-block">
+        <div className="d-flex gap-3 align-items-center">
+          <img
+            src={loginedUser.avatar ? loginedUser.avatar : ""}
+            alt=""
+            className="logoAva1 rounded-circle"
+          />
+          <p className="m-0 fw-bold">
+            {loginedUser.name ? loginedUser.name : ""}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TopInfo;
